29b782d88883c78f444d9372d044a109a6e17c63
Douglas.Goncalves <douglas.goncalves@gpnet.com.br>
2022-06-22 12:43:47 -0300

    Foi adicionado e configurado a dependencia do springfox 3.0.0 (Swagger)

---
+++ src/main/java/br/com/aceleragep/api_biblioteca/configs/swagger/SwaggerConfigurations.java
@@ -0,0 +1,22 @@
+package br.com.aceleragep.api_biblioteca.configs.swagger;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+public class SwaggerConfigurations {
+	@Bean
+	public Docket aceleraBibliotecaApi() {
+		return new Docket(DocumentationType.OAS_30)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("br.com.aceleragep.api_biblioteca"))
+				.paths(PathSelectors.ant("/**"))
+				.build();
+				//.ignoredParameterType(Usuarios.class) <<-- Usar mais tarde;
+	}
+}
